3226999c7d8a204b32a5a0cb906320541741ee63
Ivan <ivandonyk@gmail.com>
2022-09-29 23:17:39 +0300

    fix: storybook preview to use the mui theme provider so as allow injecting theme variable

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { addDecorator } from "@storybook/react"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material"
+
+import theme from "../src/theme"
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {story()}
+  </ThemeProvider>
+))
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
